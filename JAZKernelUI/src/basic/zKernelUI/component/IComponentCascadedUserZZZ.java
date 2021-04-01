d533fe9792e56cda80d1ad4691790b31e50cb6ec
firak01 <firak01@t-online.de>
2021-04-01 09:12:13 +0200

    in Arbeit: Übergib FlagZ - Werte als Programmargument, die dann an passender Stelle gesetzt werden Hier: DEBUGUI
    Signed-off-by: firak01 <firak01@t-online.de>

--- JAZKernelUI/src/basic/zKernelUI/component/IComponentCascadedUserZZZ.java
+++ JAZKernelUI/src/basic/zKernelUI/component/IComponentCascadedUserZZZ.java
@@ -5,7 +5,9 @@ import javax.swing.JComponent;
 import basic.zKernel.IKernelUserZZZ;
 
 public interface IComponentCascadedUserZZZ extends IKernelUserZZZ{
-
+	public enum FLAGZ{
+		DEBUGUI;
+	}
 	public abstract JComponent getComponent(String sAlias);
 	public abstract void setComponent(String sAlias, JComponent objComponent);
 
