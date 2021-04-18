d32ea5336abac6ccdb6180ab4ff4b92bf7ef3a6e
firak01 <firak01@t-online.de>
2021-04-18 11:38:58 +0200

    Problem: Wenn Klassen aus anderen Projekten und darin genutzten Bibliotheken stammen.
    Signed-off-by: firak01 <firak01@t-online.de>

--- JAZKernelUI/src/basic/zKernelUI/thread/KernelSwingWorkerZZZ.java
+++ JAZKernelUI/src/basic/zKernelUI/thread/KernelSwingWorkerZZZ.java
@@ -1,13 +1,43 @@
 package basic.zKernelUI.thread;
 
+
 import basic.zBasic.ExceptionZZZ;
 import basic.zBasic.IObjectZZZ;
 import basic.zBasicUI.thread.SwingWorker;
+
+//TODO: Mal auf den anderen, neueren umstellen.
+//import javax.swing.SwingWorker;
+
 import basic.zKernel.IKernelUserZZZ;
 import basic.zKernel.IKernelZZZ;
 import basic.zKernel.KernelLogZZZ;
 import custom.zKernel.LogZZZ;
 
+
+/** Abstrakte Klasse, die ZKernel-Funktionalität für Programme (Button-Clicks, etc.) zur Verfügung stellt.
+ *  Diese laufen dann in einem eigenen Thread. 
+ *  Der Code für das Program wird im der construct() - Methode implementiert.
+ *  
+ *  ##########################
+ *  Merke 20210418:
+ *  Wenn ein Program eine Klasse aus einer anderen Bibliothek nutzt, 
+ *  dann kann es sein, dass dies beim Compilieren funktioniert, beim Ausführen aber folgenden Fehler erzeugt:
+ *  ClassNotFoundException
+ *  
+ *  Z.B.: Im JAZWebFTP-Projekt. Wird JSchException aus com.jcraft.jsch nicht gefunden (das ist eine Bibliothek zum Arbeiten mit SFTP, etc.),
+ *        wenn man das Projekt in einem anderen Projekt nutzt.
+ *  
+ *  Eine reine Export-Anweisung für die MavenDependencies des Projekts reicht nicht.
+ *  
+ *   Lösung (leider nur ein Workaround): 
+ *   Binde (z.B. über Maven) diese Bibliothek mit der im Classpath nicht gefundenene Klasse
+ *   explizit auch in dem übergeordneten Projekt ein.
+ *   
+ *  #########################
+ * 
+ * @author Fritz Lindhauer, 18.04.2021, 09:33:38
+ * 
+ */
 public abstract class KernelSwingWorkerZZZ extends SwingWorker  implements IObjectZZZ, IKernelUserZZZ{
 	protected IKernelZZZ objKernel = null;
 	protected LogZZZ objLog = null;
@@ -73,5 +103,16 @@ public abstract class KernelSwingWorkerZZZ extends SwingWorker  implements IObje
 		}else {
 			objLog.WriteLineDate(sLog);
 		}		
-	}	
+	}
+	
+	//+++++++++++++++++++++++++++++++++
+	//wenn der Kerneleigene SwingWorker durch javax -Version ersetzt wurde
+//	public void start() {
+//		this.run();
+//	}
+//	
+//	@Override
+//	protected Object doInBackground() throws Exception {
+//		this.start();
+//	}	
 }
