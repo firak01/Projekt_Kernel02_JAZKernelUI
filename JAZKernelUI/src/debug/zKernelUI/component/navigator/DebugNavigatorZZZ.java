62fd4357e6c2e2637a7c406331097f7100d76617
firak01 <firak01@t-online.de>
2024-10-11 12:57:59 +0200

    Änderungen im Kernel
    Signed-off-by: firak01 <firak01@t-online.de>

--- JAZKernelUI/src/debug/zKernelUI/component/navigator/DebugNavigatorZZZ.java
+++ JAZKernelUI/src/debug/zKernelUI/component/navigator/DebugNavigatorZZZ.java
@@ -2,10 +2,13 @@ package debug.zKernelUI.component.navigator;
 
 import basic.zBasic.ExceptionZZZ;
 import basic.zKernel.KernelZZZ;
+import basic.zKernel.file.ini.IIniTagWithExpressionZZZ;
 import basic.zKernel.file.ini.IKernelExpressionIniSolverZZZ;
 import basic.zKernel.file.ini.IKernelJsonArrayIniSolverZZZ;
 import basic.zKernel.file.ini.IKernelJsonIniSolverZZZ;
 import basic.zKernel.file.ini.IKernelJsonMapIniSolverZZZ;
+import basic.zKernel.file.ini.IKernelZFormulaIni_PathZZZ;
+import basic.zKernel.file.ini.IKernelZFormulaIni_VariableZZZ;
 import custom.zKernel.LogZZZ;
 import custom.zKernelUI.module.config.FrameConfigZZZ;
 
@@ -14,7 +17,7 @@ public class DebugNavigatorZZZ {
 	public static void main(String[] args) {
 		try {
 			//1. Kernel Objekt initialisieren. Dies ist fuer das Logging grundlegend.
-			String[] saFlag= {"DEBUGUI_PANELLABEL_ON","useFormula", IKernelExpressionIniSolverZZZ.FLAGZ.USEEXPRESSION.name(), IKernelJsonIniSolverZZZ.FLAGZ.USEJSON.name(), IKernelJsonArrayIniSolverZZZ.FLAGZ.USEJSON_ARRAY.name(), IKernelJsonMapIniSolverZZZ.FLAGZ.USEJSON_MAP.name()};
+			String[] saFlag= {"DEBUGUI_PANELLABEL_ON","useFormula", IKernelZFormulaIni_PathZZZ.FLAGZ.USEEXPRESSION_PATH.name(), IKernelZFormulaIni_VariableZZZ.FLAGZ.USEEXPRESSION_VARIABLE.name(), IIniTagWithExpressionZZZ.FLAGZ.USEEXPRESSION.name(), IKernelExpressionIniSolverZZZ.FLAGZ.USEEXPRESSION_SOLVER.name(), IKernelJsonIniSolverZZZ.FLAGZ.USEJSON.name(), IKernelJsonArrayIniSolverZZZ.FLAGZ.USEJSON_ARRAY.name(), IKernelJsonMapIniSolverZZZ.FLAGZ.USEJSON_MAP.name()};
 			KernelZZZ objKernel = new KernelZZZ("FGL", "01", "", "ZKernelConfigConfig_default.ini", saFlag);
 			LogZZZ objLog = objKernel.getLogObject();
 			
